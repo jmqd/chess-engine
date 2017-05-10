40287c1f90c6e8384ab051d4197cac08a26c48f8
Jordan McQueen <dojo3@uw.edu>
2017-05-09 22:42:54 -0700

    introduce concept of computer and rewinding. slight refactors.

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import sys
 import copy
 import string
 import random
@@ -16,8 +17,10 @@ from piece import Pawn
 from piece import Color
 from piece import EmptySquare
 
-from util import humanize_square_name
-from util import dehumanize_square_name
+from engine import ChessEngine
+
+from util import to_algebraic
+from util import to_numeric
 from util import get_move_facts
 from util import A_THRU_H
 
@@ -32,7 +35,9 @@ STANDARD_STARTING_POSITION = [
     'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P',
     'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'
         ]
+
 EMPTY_BOARD = [' '] * 64
+
 VERTICAL_STEP = 8
 HORIZONTAL_STEP = 1
 PIECE_TYPES = [King, Queen, Pawn, Rook, Knight, Bishop]
@@ -46,22 +51,28 @@ class Game:
         self.captured_pieces = []
         self.is_playing = True
 
+        # TODO: break this out into a Computer class
+        self.computer = None
+        self.is_computer_playing = None
+        self.computer_color = None
+
     def next_player(self):
-        self.active_player = Color.white if self.active_player == Color.black else Color.black
+        return Color.white if self.active_player == Color.black else Color.black
 
     @property
     def inactive_player(self):
         return Color.white if self.active_player == Color.black else Color.white
 
     def legal_moves_for_square(self, square):
-        square = square if type(square) == int else dehumanize_square_name(square)
+        square = square if type(square) == int else to_numeric(square)
         return self.position[square].legal_move_strategy(self, square).get_legal_moves()
 
     def find_all_legal_moves(self):
-        legal_moves = set()
+        legal_moves = []
         for i, piece in enumerate(self.position):
             if piece.__class__ == EmptySquare or piece.color != self.active_player: continue
-            legal_moves |= set(humanize_square_name(i) + '->' + humanize_square_name(x) for x in self.legal_moves_for_square(i))
+            for legal_move in self.legal_moves_for_square(i):
+                legal_moves.append((i, legal_move))
         return legal_moves
 
 
@@ -74,7 +85,7 @@ class Game:
 
     def move(self, origin, destination):
         piece = self.position[origin]
-        new_square = destination if type(destination) == int else dehumanize_square_name(destination)
+        new_square = destination if type(destination) == int else to_numeric(destination)
 
         if not isinstance(piece, Piece):
             raise ValueError("That square is empty!")
@@ -87,25 +98,58 @@ class Game:
         if new_square not in legal_moves:
             raise ValueError("You can't move that there!")
 
+        captured_piece = None
+
         if self.is_occupied(new_square):
-            self.captured_pieces.append(self.position[new_square])
+            captured_piece = self.position[new_square]
+            self.captured_pieces.append(captured_piece)
 
         self.position[origin], self.position[new_square] = EmptySquare(), piece
-        print("Moved the {} at {} to {}.".format(piece.name, origin, destination))
-        self.move_history.append((origin, destination))
-        self.next_player()
+
+        logging.info("Moved the {} at {} to {}.".format(piece.name, origin, destination))
+
+        self.move_history.append((origin, destination, captured_piece))
+        self.active_player = self.next_player()
+
+    def start_engine(self):
+        self.computer = ChessEngine(self)
+
+    def rewind(self):
+        origin, destination, captured_piece = self.move_history.pop()
+        self.position[origin], self.position[destination] = self.position[destination], captured_piece or EmptySquare()
+
+        logging.info("Rewinded move {} to {}.".format(to_algebraic(origin), to_algebraic(destination)))
+
+        self.active_player = self.next_player()
+
+    def prompt_for_mode(self):
+        self.is_computer_playing = True if input("Play against the computer? y/n > ") == "y" else False
+        self.computer_color = Color.white if input("Choose black or white b/w > ") == 'b' else Color.black
+        self.start_engine()
 
     @staticmethod
     def play():
         game = Game()
+        game.prompt_for_mode()
+
         while game.is_playing:
             tmp_function_print_squares_for_pieces(game)
+            if game.is_computer_playing and game.computer_color == game.active_player:
+                origin, destination = game.computer.choose_random_move()
+                game.move(origin, destination)
+
             game.show()
             move = input("{} to move: ".format(game.active_player.name))
+
             if move == 'listAll':
                 print(game.find_all_legal_moves())
+            elif move == 'rewind':
+                game.rewind()
+            elif move in ('quit', 'q'):
+                print("Quitting...")
+                sys.exit(0)
             else:
-                origin, destination = move.split('->')
+                origin, destination = move.split()
                 try:
                     game.move(origin, destination)
                 except Exception as e:
@@ -117,8 +161,6 @@ class Game:
     def is_occupied(self, square):
         return not self.is_empty(square)
 
-    def is_attacked_by_active_player(square):
-        return square in self.attacked_squares
 
     def is_empty(self, square):
         logging.debug("Checking if %s is empty...", square)
@@ -155,7 +197,7 @@ def tmp_function_print_squares_for_pieces(game):
     print("Randomly decided to show all positions of {} {}s".format(color_to_check.name,
                                                                     piece_to_check.name),
           end = ': ')
-    print([humanize_square_name(x) for x in squares])
+    print([to_algebraic(x) for x in squares])
 
 class Position:
     def __init__(self, position_data):
@@ -203,10 +245,10 @@ class Position:
 
 
     def __getitem__(self, square):
-        index = square if type(square) == int else dehumanize_square_name(square)
+        index = square if type(square) == int else to_numeric(square)
         return self.position[index]
 
     def __setitem__(self, square, value):
-        index = square if type(square) == int else dehumanize_square_name(square)
+        index = square if type(square) == int else to_numeric(square)
         self.position[index] = value
 
