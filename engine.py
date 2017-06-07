ec0f7f56f26a6f5e274810143302e22f9cd684bd
Jordan McQueen <dojo3@uw.edu>
2017-06-07 05:40:21 -0700

    beginning feature work for positional eval

--- engine.py
+++ engine.py
@@ -3,6 +3,13 @@ import operator
 import logging
 
 from piece import Color
+from piece import Pawn
+from piece import King
+from piece import Rook
+from piece import Bishop
+from piece import Queen
+from piece import Knight
+
 from position import Position
 
 EVEN_EVALUATION = 0.0
@@ -17,18 +24,19 @@ class ChessEngine:
         return move
 
     @staticmethod
-    def evaluate_position_materially(position: Position) -> float:
+    def evaluate_position(position: Position) -> float:
         # TODO: continue implementing
         evaluation = EVEN_EVALUATION
         for square in position:
-            if square.is_empty():
-                logging.info("empty square")
-                continue
-            if square.piece.color == Color.white:
-                logging.info('white %s, adding %s', square.piece, square.piece.value)
-                evaluation += square.piece.value
-            if square.piece.color == Color.black:
-                logging.info('black %s, subtracting %s', square.piece, square.piece.value)
-                evaluation -= square.piece.value
+            if square.is_empty(): continue
+            reckon = operator.__iadd__ if square.piece.color == Color.white else operator.__isub__
+            reckon(evaluation, square.piece.value)
+            # TODO: implement positional evaluations
+            # reckon(evaluation, evaluate_positionally(square))
         return evaluation
 
+def evaluate_positionally(square: 'Square') -> float:
+    evaluation = EVEN_EVALUATION
+    if square.piece.__class__ == Pawn:
+        # TODO: do the right weighting math here
+        evaluation += square.piece.distance_from_promotion(square.numeric_index)
