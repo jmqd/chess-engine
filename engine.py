2f8b6adbdb067509fc3c51086226dfed6146f72f
Jordan McQueen <dojo3@uw.edu>
2017-06-06 22:34:22 -0700

    material evaluation

--- engine.py
+++ engine.py
@@ -1,9 +1,11 @@
 import random
 import operator
+import logging
 
 from piece import Color
 from position import Position
 
+EVEN_EVALUATION = 0.0
 
 class ChessEngine:
     def __init__(self, game: object) -> None:
@@ -15,10 +17,18 @@ class ChessEngine:
         return move
 
     @staticmethod
-    def evaluate_position(position: Position) -> float:
+    def evaluate_position_materially(position: Position) -> float:
         # TODO: continue implementing
-        for square in positon:
-            if square.is_empty(): continue
-
-
+        evaluation = EVEN_EVALUATION
+        for square in position:
+            if square.is_empty():
+                logging.info("empty square")
+                continue
+            if square.piece.color == Color.white:
+                logging.info('white %s, adding %s', square.piece, square.piece.value)
+                evaluation += square.piece.value
+            if square.piece.color == Color.black:
+                logging.info('black %s, subtracting %s', square.piece, square.piece.value)
+                evaluation -= square.piece.value
+        return evaluation
 
