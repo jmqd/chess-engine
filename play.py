d7b522a0b0f24f763293e216c07ad8a9f2bc0d22
Jordan McQueen <dojo3@uw.edu>
2017-06-12 05:13:22 -0700

    adding different modes and cli tools/util

--- play.py
+++ play.py
@@ -1,10 +1,53 @@
 import logging
+import click
 
 from src.game import Game
+from src.position import Position
+from src.game import STANDARD_STARTING_POSITION
+from src.piece import Color
 
-def main() -> None:
-    # logging.basicConfig(level = logging.INFO)
-    Game.play()
+KNIGHT_FORK_POSITION = [
+    ' ', ' ', ' ', ' ', ' ', 'n', ' ', ' ',
+    ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+    ' ', 'r', ' ', ' ', ' ', ' ', ' ', ' ',
+    ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+    ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+    'k', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+    ' ', ' ', ' ', 'N', ' ', 'K', ' ', ' ',
+    ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '
+        ]
+
+STARTING_POSITIONS = {
+        'standard': STANDARD_STARTING_POSITION,
+        'knight_fork': KNIGHT_FORK_POSITION
+        }
+
+@click.group('cli')
+def cli() -> None: pass
+
+@cli.command('play')
+@click.option('--starting-position', type = str, required = False, default = 'standard')
+@click.option('--color', type = str, required = False, default = 'white')
+def play(starting_position: str, color: str) -> None:
+    if color not in ('black', 'white'):
+        raise ValueError("Color must be black or white.")
+
+    starting_color = Color.white if color == 'white' else Color.black
+    position = STARTING_POSITIONS[starting_position]
+    Game.play(position, starting_color)
+
+@cli.command('show-legal-moves')
+@click.option('--starting-position', type = str, required = False, default = 'standard')
+@click.option('--color', type = str, required = False, default = 'white')
+def show_legal_moves(starting_position: str, color: str) -> None:
+    if color not in ('black', 'white'):
+        raise ValueError("Color must be black or white.")
+
+    starting_color = Color.white if color == 'white' else Color.black
+    position = Position(STARTING_POSITIONS[starting_position], starting_color)
+
+    print(position)
+    print([str(x) for x in position.find_all_legal_moves()])
 
 if __name__ == '__main__':
-    main()
+    cli()
